15ff03053b993f4849c4c9d632c99d1cda28f166
Majharul Islam <majharul@codevaly.com>
2026-02-08 17:59:58 +0600

    Removed JUNK

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,15 +26,6 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
-        }}
-      />
     </Tabs>
   );
 }
